b9c34e584d6030860e9c549432ae6aead87cc315
teacher-kiwi <kwi07@hanmail.net>
2023-04-13 18:16:47 +0900

    채팅기능

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 require("dotenv").config();
 
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello Qoddi!, I'm kiwi!");
+app.use(express.static("public"));
+
+app.get("/*", (req, res) => res.redirect("/"));
+
+const usedNickname = {};
+io.on("connection", (socket) => {
+  socket.on("enterRoom", (nickname, sendAlert) => {
+    if (usedNickname[nickname]) {
+      sendAlert(false);
+    } else {
+      sendAlert(true);
+      usedNickname[nickname] = true;
+      socket["nickname"] = nickname;
+      socket.join("room");
+      io.to("room").emit("talk", { id: Date(), nickname, msg: `${nickname}님 께서 들어오셨습니다.` });
+    }
+  });
+
+  socket.on("talk", ({ nickname, msg }) => {
+    io.to("room").emit("talk", { id: Date(), nickname, msg });
+  });
+
+  socket.on("disconnecting", () => {
+    if (socket.nickname) {
+      delete usedNickname[socket.nickname];
+      socket.to("room").emit("talk", { id: Date(), nickname: socket.nickname, msg: `${socket.nickname}님께서 나가셨습니다.` });
+    }
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(PORT);
-});
+server.listen(PORT, () => console.log(PORT));

6bd8916f783e16bb11dfc961f4f2c5679b7e2e76
teacher-kiwi <kwi07@hanmail.net>
2023-05-03 00:23:47 +0900

    채팅 기능 추가

--- index.js
+++ index.js
@@ -11,28 +11,102 @@ app.use(express.static("public"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const usedNickname = {};
+const roomList = {};
 io.on("connection", (socket) => {
-  socket.on("enterRoom", (nickname, sendAlert) => {
+  socket.on("makeName", (nickname, ok) => {
     if (usedNickname[nickname]) {
-      sendAlert(false);
+      ok(false);
     } else {
-      sendAlert(true);
+      ok(true);
       usedNickname[nickname] = true;
       socket["nickname"] = nickname;
-      socket.join("room");
-      io.to("room").emit("talk", { id: Date(), nickname, msg: `${nickname}님 께서 들어오셨습니다.` });
+    }
+
+    io.emit("userCount", { count: Object.keys(usedNickname).length });
+    socket.emit("roomList", { roomList: Object.keys(roomList) });
+  });
+
+  socket.on("makeRoom", (roomName, ok) => {
+    if (roomList[roomName]) {
+      ok(false);
+    } else {
+      ok(true);
+      socket["roomName"] = roomName;
+      const nickname = socket.nickname;
+      roomList[roomName] = nickname;
+      socket.join(roomName);
+
+      io.to(roomName).emit(
+        "updateUser",
+        [...io.sockets.adapter.rooms.get(roomName)].map((id) => io.sockets.sockets.get(id).nickname)
+      );
+      io.to(roomName).emit("talk", { id: Date(), nickname: "키위🥝", msg: `${nickname}님께서 들어오셨습니다.` });
+    }
+
+    io.emit("roomList", { roomList: Object.keys(roomList) });
+  });
+
+  socket.on("enterRoom", (roomName, ok) => {
+    if (roomList[roomName]) {
+      ok(true);
+      socket["roomName"] = roomName;
+      const nickname = socket.nickname;
+      socket.join(roomName);
+      io.to(roomName).emit(
+        "updateUser",
+        [...io.sockets.adapter.rooms.get(roomName)].map((id) => io.sockets.sockets.get(id).nickname)
+      );
+      io.to(roomName).emit("talk", { id: Date(), nickname: "키위🥝", msg: `${nickname}님께서 들어오셨습니다.` });
+    } else {
+      ok(false);
     }
   });
 
-  socket.on("talk", ({ nickname, msg }) => {
-    io.to("room").emit("talk", { id: Date(), nickname, msg });
+  socket.on("outRoom", (roomName) => {
+    if (socket.nickname === roomList[roomName]) {
+      // 방 폭파
+      io.to(roomName).emit("deleteRoom", roomName);
+      io.socketsLeave(roomName);
+      delete roomList[roomName];
+      io.emit("roomList", { roomList: Object.keys(roomList) });
+    } else {
+      // 방 나가기
+      socket.leave(roomName);
+      io.to(roomName).emit(
+        "updateUser",
+        [...io.sockets.adapter.rooms.get(roomName)].map((id) => io.sockets.sockets.get(id).nickname)
+      );
+      io.to(roomName).emit("talk", { id: Date(), nickname: "키위🥝", msg: `${socket.nickname}님께서 나가셨습니다.` });
+    }
+  });
+
+  socket.on("talk", ({ msg }) => {
+    io.to(socket.roomName).emit("talk", { id: Date(), nickname: socket.nickname, msg });
   });
 
   socket.on("disconnecting", () => {
     if (socket.nickname) {
-      delete usedNickname[socket.nickname];
-      socket.to("room").emit("talk", { id: Date(), nickname: socket.nickname, msg: `${socket.nickname}님께서 나가셨습니다.` });
+      const nickname = socket.nickname;
+      delete usedNickname[nickname];
+      if (socket.roomName) {
+        const roomName = socket.roomName;
+        if (nickname === roomList[roomName]) {
+          io.to(roomName).emit("deleteRoom", roomName);
+          io.socketsLeave(roomName);
+          delete roomList[roomName];
+          io.emit("roomList", { roomList: Object.keys(roomList) });
+        } else {
+          socket.to(roomName).emit("talk", { id: Date(), nickname: "키위🥝", msg: `${nickname}님께서 나가셨습니다.` });
+          socket.leave(roomName);
+          io.to(roomName).emit(
+            "updateUser",
+            [...io.sockets.adapter.rooms.get(roomName)].map((id) => io.sockets.sockets.get(id).nickname)
+          );
+        }
+      }
     }
+
+    io.emit("userCount", { count: Object.keys(usedNickname).length });
   });
 });
 
